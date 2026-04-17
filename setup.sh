a338913f9ce3034fddb58604c84cb5762b585dcb
Komal1github <105955558+Komal1github@users.noreply.github.com>
2026-04-17 10:54:03 +0200

    Add setup script for AWS EC2 Nginx deployment
    Documented the process of deploying a web server on AWS EC2 using Nginx.

---
+++ setup.sh
@@ -0,0 +1,18 @@
+# AWS EC2 Web Server Deployment (Nginx)
+
+This project demonstrates deploying a web server on AWS EC2 using Nginx.
+
+## Summary
+- Launched EC2 instance (Amazon Linux 2 AMI, instance type t3.micro( eligible free tier)
+- Configured SSH (port 22) and HTTP (port 80) access
+- Installed and started Nginx
+- Deployed and tested web server via public IP
+
+## Key Skills
+- AWS EC2
+- Linux basics
+- Nginx web server
+- Cloud networking (Security Groups)
+
+## Result
+Web server successfully accessible via browser using EC2 public IP.
